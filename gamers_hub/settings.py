57e31d08117b075bc78e75c6029843249ff297c2
Mayur Gajbhiye <mayurgajbhiye4@gmail.com>
2024-12-19 16:58:07 +0530

    final updates

--- gamers_hub/settings.py
+++ gamers_hub/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER': config('DB_USERNAME'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '5432'          
     }
 }
 
