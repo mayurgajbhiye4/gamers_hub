f0c152e819a47215be69db651056698d12eac1a4
Mayur Gajbhiye <mayurgajbhiye4@gmail.com>
2024-12-19 16:54:02 +0530

    final updates

--- gamers_hub/settings.py
+++ gamers_hub/settings.py
@@ -22,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!rfc9ntz434nq^48gksax!26la6wlt%qji-q3664k=$&0gots'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False    
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
